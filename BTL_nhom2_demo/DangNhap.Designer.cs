ba719ff69e11cb5de982af32049e8b043816de94
dnghia2109 <laiduynghia21@gmail.com>
2022-12-11 21:35:44 +0700

    test

--- BTL_nhom2_demo/DangNhap.Designer.cs
+++ BTL_nhom2_demo/DangNhap.Designer.cs
@@ -39,69 +39,77 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(116, 84);
+            this.label1.Location = new System.Drawing.Point(63, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(145, 25);
+            this.label1.Size = new System.Drawing.Size(81, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Tên đăng nhập";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(116, 195);
+            this.label2.Location = new System.Drawing.Point(63, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 25);
+            this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Mật khẩu";
             // 
             // textBox1
             // 
             this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
-            this.textBox1.Location = new System.Drawing.Point(312, 84);
+            this.textBox1.Location = new System.Drawing.Point(170, 46);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(300, 40);
+            this.textBox1.Size = new System.Drawing.Size(165, 24);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
             this.textBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
-            this.textBox2.Location = new System.Drawing.Point(312, 195);
+            this.textBox2.Location = new System.Drawing.Point(170, 106);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(300, 40);
+            this.textBox2.Size = new System.Drawing.Size(165, 24);
             this.textBox2.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(312, 282);
+            this.button1.Location = new System.Drawing.Point(170, 153);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(130, 55);
+            this.button1.Size = new System.Drawing.Size(71, 30);
             this.button1.TabIndex = 4;
             this.button1.Text = "Thoát";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(482, 282);
+            this.button2.Location = new System.Drawing.Point(263, 153);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(130, 55);
+            this.button2.Size = new System.Drawing.Size(71, 30);
             this.button2.TabIndex = 5;
             this.button2.Text = "Đăng nhập";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // Form1
+            // DangNhap
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(771, 384);
+            this.ClientSize = new System.Drawing.Size(421, 208);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "DangNhap";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
